c64a358a07aaa9c832f88eb146322231fd035977
Alexander <ashlinchak@gmail.com>
2016-03-10 21:28:48 +0200

    Change a configuration logic

--- config/index.js
+++ config/index.js
@@ -2,8 +2,10 @@
 
 var nconf = require('nconf');
 
+var env = process.env.NODE_ENV || 'development';
+
 nconf.argv().env().file({
-  file: process.cwd() + '/config/options.json',
+  file: process.cwd() + '/config/' + env + '.json',
 });
 
 module.exports = nconf;
