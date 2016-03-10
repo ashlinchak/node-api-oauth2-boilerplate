66e317e855d7e7648fece74681bc354be33ac5c9
Alexander <ashlinchak@gmail.com>
2016-03-10 22:02:03 +0200

    Add defaults configs

--- config/index.js
+++ config/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 var nconf = require('nconf');
+var fs = require('fs');
+
+var defaults = JSON.parse(fs.readFileSync(process.cwd() + '/config/defaults.json'));
 
 var env = process.env.NODE_ENV || 'development';
 
 nconf.argv().env().file({
   file: process.cwd() + '/config/' + env + '.json',
 });
+
+nconf.defaults(defaults);
 
 module.exports = nconf;
