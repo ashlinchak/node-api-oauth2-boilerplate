ed0530c5bfeb248ced91be2e2c349758eb0f50b8
Alexander <ashlinchak@gmail.com>
2016-03-11 20:58:57 +0200

    Impovements in configurations

--- config/index.js
+++ config/index.js
@@ -3,13 +3,25 @@
 var nconf = require('nconf');
 var fs = require('fs');
 
-var defaults = JSON.parse(fs.readFileSync(process.cwd() + '/config/defaults.json'));
-
 var env = process.env.NODE_ENV || 'development';
+var defaultConfigPath = null;
+var envConfigPath = null;
 
-nconf.argv().env().file({
-  file: process.cwd() + '/config/' + env + '.json',
-});
+if (process.NODE_ENV == 'production') {
+  defaultConfigPath = process.cwd() + 'current/config/defaults.json';
+} else {
+  defaultConfigPath = process.cwd() + '/config/defaults.json';
+}
+
+if (process.NODE_ENV == 'production') {
+  envConfigPath = process.cwd() + '/current/config/' + env + '.json';
+} else {
+  envConfigPath = process.cwd() + '/config/' + env + '.json';
+}
+
+var defaults = JSON.parse(fs.readFileSync(defaultConfigPath));
+
+nconf.argv().env().file({ file: envConfigPath });
 
 nconf.defaults(defaults);
 
