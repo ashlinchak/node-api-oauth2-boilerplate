7c63ed67c6cbd4b1d011eb29c94e668ca31a67c5
Alexander <ashlinchak@gmail.com>
2016-03-13 00:38:37 +0200

    Improvements in configs

--- config/index.js
+++ config/index.js
@@ -2,27 +2,14 @@
 
 var nconf = require('nconf');
 var fs = require('fs');
+var path = require('path');
 
 var env = process.env.NODE_ENV || 'development';
-var defaultConfigPath = null;
-var envConfigPath = null;
-
-if (process.env.NODE_ENV == 'production') {
-  defaultConfigPath = process.cwd() + 'current/config/defaults.json';
-} else {
-  defaultConfigPath = process.cwd() + '/config/defaults.json';
-}
-
-if (process.env.NODE_ENV == 'production') {
-  envConfigPath = process.cwd() + '/current/config/' + env + '.json';
-} else {
-  envConfigPath = process.cwd() + '/config/' + env + '.json';
-}
-
+var defaultConfigPath = path.join(__dirname, 'defaults.json');
+var envConfigPath = path.join(__dirname, env + '.json');
 var defaults = JSON.parse(fs.readFileSync(defaultConfigPath));
 
 nconf.argv().env().file({ file: envConfigPath });
-
 nconf.defaults(defaults);
 
 module.exports = nconf;
